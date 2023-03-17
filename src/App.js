05a82e84dcb590f4a5d14ca87f87182fc544bb62
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-03-17 10:19:53 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ErrorBoundary from './components/ErrorBoundary';
+
+function BuggyComponent() {
+  const obj = undefined;
+  return <div>{obj.name}</div>; // This will cause an error
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Error Boundary Example</h1>
+        <ErrorBoundary>
+          <BuggyComponent />
+        </ErrorBoundary>
     </div>
   );
 }
